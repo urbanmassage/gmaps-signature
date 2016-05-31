62b5ed364f0637b9d92f0394da4c8172c53869d0
Jessia Salmon <jessicasalmon05@gmail.com>
2016-05-31 21:12:38 +0100

    TBD as to where to place - temp file

---
+++ wheretoput.ts
@@ -0,0 +1,11 @@
+module.exports = function () {}
+
+require('myfile')
+
+
+export function myFunction () {}
+module.exports.myFunction = function () {}
+
+
+import React from 'react';
+var React = require('react').default;
